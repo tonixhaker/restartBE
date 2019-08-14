cb3dd845e94103bdd44f5d7457cc9a99dada7dec
tonixhaker <tonixhaker@gmail.com>
2019-08-14 16:51:39 +0300

    Basic passport-local auth

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('./config/database');
+const passport = require('./config/passport');
+const bodyParser = require('body-parser');
 const { EXPRESS_PORT } = require('./config/app');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(require('./routes'));
 app.listen(EXPRESS_PORT, () => console.log(`Ready on port ${EXPRESS_PORT}!`));
