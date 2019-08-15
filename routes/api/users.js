12a1fcf668bf39efb25f585267df6ce4803ac7f9
tonixhaker <tonixhaker@gmail.com>
2019-08-15 16:43:19 +0300

    handle error with try/catch

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,41 +14,49 @@ router.post('/register', auth.optional, async(req, res, next) => {
 
         await finalUser.save();
         res.json({user: finalUser.toAuthJSON()});
+
     } catch (e) {
         res.status(500).send(e);
     }
 });
 
 router.post('/login', auth.optional, (req, res, next) => {
-    const { body: { user } } = req;
+    try {
 
-    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-        if(err) {
-            return next(err);
-        }
+        return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+            if(err) {
+                return next(err);
+            }
 
-        if(passportUser) {
-            const user = passportUser;
-            user.token = passportUser.generateJWT();
+            if(passportUser) {
+                const user = passportUser;
+                user.token = passportUser.generateJWT();
 
-            return res.json({ user: user.toAuthJSON() });
-        }
+                return res.json({ user: user.toAuthJSON() });
+            }
+            return status(400).info;
+        })(req, res, next);
 
-        return status(400).info;
-    })(req, res, next);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 router.get('/current', auth.required, (req, res, next) => {
-    const { payload: { id } } = req;
+    try {
+        const { payload: { id } } = req;
 
-    return Users.findById(id)
-        .then((user) => {
-            if(!user) {
-                return res.sendStatus(400);
-            }
+        return Users.findById(id)
+            .then((user) => {
+                if(!user) {
+                    return res.sendStatus(400);
+                }
 
-            return res.json({ user: user.toAuthJSON() });
-        });
+                return res.json({ user: user.toAuthJSON() });
+            });
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 module.exports = router;
